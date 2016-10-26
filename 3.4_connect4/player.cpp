3a3fac3bd119edc93ccb8635c50cf2f3aa0a19c2
Sandro Wirth <s.wirth303@gmail.com>
2016-10-26 16:35:50 +0200

    interface for computer player added -> no functionality yet

---
+++ 3.4_connect4/player.cpp
@@ -0,0 +1,17 @@
+#ifndef ADV_CPP_HS16_PLAYER_H
+#define ADV_CPP_HS16_PLAYER_H
+
+template<typename F>
+class player
+{
+	public:
+		int play(const F &field);
+};
+
+template<typename F>
+int player::play(const F &field)
+{
+	return 0;
+}
+
+#endif //ADV_CPP_HS16_PLAYER_H
